51c516411de3bfdf6e2cf0da9ea0f8f8eedd28f3
Anton <45803223+antonius21@users.noreply.github.com>
2023-10-19 14:51:47 +0300

    Creating role system

--- GreenApp/View/EnterForm.Designer.cs
+++ GreenApp/View/EnterForm.Designer.cs
@@ -29,69 +29,70 @@
         private void InitializeComponent()
         {
             panel1 = new Panel();
-            label1 = new Label();
-            label2 = new Label();
-            button1 = new Button();
-            textBox1 = new TextBox();
-            textBox2 = new TextBox();
+            passwordTextBox = new TextBox();
+            loginTextBox = new TextBox();
+            enterButton = new Button();
+            passwordLabel = new Label();
+            loginLabel = new Label();
             panel1.SuspendLayout();
             SuspendLayout();
             // 
             // panel1
             // 
             panel1.BackColor = Color.White;
-            panel1.Controls.Add(textBox2);
-            panel1.Controls.Add(textBox1);
-            panel1.Controls.Add(button1);
-            panel1.Controls.Add(label2);
-            panel1.Controls.Add(label1);
+            panel1.Controls.Add(passwordTextBox);
+            panel1.Controls.Add(loginTextBox);
+            panel1.Controls.Add(enterButton);
+            panel1.Controls.Add(passwordLabel);
+            panel1.Controls.Add(loginLabel);
             panel1.Location = new Point(-2, -1);
             panel1.Name = "panel1";
             panel1.Size = new Size(804, 451);
             panel1.TabIndex = 0;
             // 
-            // label1
+            // passwordTextBox
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(244, 145);
-            label1.Name = "label1";
-            label1.Size = new Size(137, 15);
-            label1.TabIndex = 0;
-            label1.Text = "Уведіть логін для входу ";
+            passwordTextBox.BackColor = Color.White;
+            passwordTextBox.Location = new Point(421, 230);
+            passwordTextBox.Name = "passwordTextBox";
+            passwordTextBox.Size = new Size(100, 23);
+            passwordTextBox.TabIndex = 4;
             // 
-            // label2
+            // loginTextBox
             // 
-            label2.AutoSize = true;
-            label2.Location = new Point(244, 233);
-            label2.Name = "label2";
-            label2.Size = new Size(148, 15);
-            label2.TabIndex = 1;
-            label2.Text = "Уведіть пароль для входу ";
+            loginTextBox.BackColor = Color.White;
+            loginTextBox.Location = new Point(421, 142);
+            loginTextBox.Name = "loginTextBox";
+            loginTextBox.Size = new Size(100, 23);
+            loginTextBox.TabIndex = 3;
             // 
-            // button1
+            // enterButton
             // 
-            button1.Location = new Point(244, 314);
-            button1.Name = "button1";
-            button1.Size = new Size(277, 40);
-            button1.TabIndex = 2;
-            button1.Text = "Увійти у систему";
-            button1.UseVisualStyleBackColor = true;
+            enterButton.Location = new Point(244, 314);
+            enterButton.Name = "enterButton";
+            enterButton.Size = new Size(277, 40);
+            enterButton.TabIndex = 2;
+            enterButton.Text = "Увійти у систему";
+            enterButton.UseVisualStyleBackColor = true;
+            enterButton.Click += enterButton_Click;
             // 
-            // textBox1
+            // passwordLabel
             // 
-            textBox1.BackColor = Color.White;
-            textBox1.Location = new Point(421, 142);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(100, 23);
-            textBox1.TabIndex = 3;
+            passwordLabel.AutoSize = true;
+            passwordLabel.Location = new Point(244, 233);
+            passwordLabel.Name = "passwordLabel";
+            passwordLabel.Size = new Size(148, 15);
+            passwordLabel.TabIndex = 1;
+            passwordLabel.Text = "Уведіть пароль для входу ";
             // 
-            // textBox2
+            // loginLabel
             // 
-            textBox2.BackColor = Color.White;
-            textBox2.Location = new Point(421, 230);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(100, 23);
-            textBox2.TabIndex = 4;
+            loginLabel.AutoSize = true;
+            loginLabel.Location = new Point(244, 145);
+            loginLabel.Name = "loginLabel";
+            loginLabel.Size = new Size(137, 15);
+            loginLabel.TabIndex = 0;
+            loginLabel.Text = "Уведіть логін для входу ";
             // 
             // EnterForm
             // 
@@ -110,10 +111,10 @@
         #endregion
 
         private Panel panel1;
-        private TextBox textBox2;
-        private TextBox textBox1;
-        private Button button1;
-        private Label label2;
-        private Label label1;
+        private TextBox passwordTextBox;
+        private TextBox loginTextBox;
+        private Button enterButton;
+        private Label passwordLabel;
+        private Label loginLabel;
     }
 }
--- GreenApp/View/EnterForm.cs
+++ GreenApp/View/EnterForm.cs
@@ -2,9 +2,42 @@ namespace GreenApp
 {
     public partial class EnterForm : Form
     {
+        string[,,] userData = new string[4, 4, 4];
+
         public EnterForm()
         {
             InitializeComponent();
+            userData[0, 0, 0] = "anton";
+            userData[0, 1, 0] = "12345";
+            userData[0, 2, 0] = "agronom";
+
+
+            userData[1, 0, 0] = "ivan";
+            userData[1, 1, 0] = "12345678";
+            userData[1, 2, 0] = "mechanizator";
+
+            userData[2, 0, 0] = "mukola";
+            userData[2, 1, 0] = "54321";
+            userData[2, 2, 0] = "economist";
         }
+
+        private void enterButton_Click(object sender, EventArgs e)
+        {
+            string userName = loginTextBox.Text;
+            string userPassword = passwordTextBox.Text;
+            string userRole = "";
+
+            if (string.IsNullOrEmpty(userName)&& string.IsNullOrEmpty(userPassword))
+            {
+                for (int i=0; i<userData.Length;i++) 
+                {
+                    if (userData[i,0,0] == userName && userData[i,1,0] == userPassword ) 
+                    { 
+                        userRole = userData[i,0,2];
+                        MessageBox.Show("Роль користувача:", userRole );
+                    }
+                }
+            }
+        } 
     }
 }
