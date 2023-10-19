333c5cc052188f8bbe779cb439aa7b57877c9e97
Anton <45803223+antonius21@users.noreply.github.com>
2023-10-19 14:28:51 +0300

    Creating user enter

--- GreenApp/View/EnterForm.Designer.cs
+++ GreenApp/View/EnterForm.Designer.cs
@@ -28,12 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            panel1 = new Panel();
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.White;
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(-2, -1);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(804, 451);
+            panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(244, 145);
+            label1.Name = "label1";
+            label1.Size = new Size(137, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Уведіть логін для входу ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(244, 233);
+            label2.Name = "label2";
+            label2.Size = new Size(148, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Уведіть пароль для входу ";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(244, 314);
+            button1.Name = "button1";
+            button1.Size = new Size(277, 40);
+            button1.TabIndex = 2;
+            button1.Text = "Увійти у систему";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.White;
+            textBox1.Location = new Point(421, 142);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = Color.White;
+            textBox2.Location = new Point(421, 230);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 4;
+            // 
+            // EnterForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
             ClientSize = new Size(800, 450);
-            Text = "Form1";
+            Controls.Add(panel1);
+            Name = "EnterForm";
+            Text = "Green ERP";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel1;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Button button1;
+        private Label label2;
+        private Label label1;
     }
 }
